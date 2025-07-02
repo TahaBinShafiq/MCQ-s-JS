14580767a5ee1966d940062a6812a9f9d3fc8da3
Taha Bin Shafiq <muhammadtahaaa7@gmail.com>
2025-07-02 20:54:22 +0500

    add result

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const questions = [
     },
     {
         question: "How many days are there in a week?",
-        options: [5, 6, 7, 8],
-        answer: 7
+        options: ["5", "6", "7", "8"],
+        answer: "7"
     },
     {
         question: "Which one is a fruit?",
         options: ["Carrot", "Apple", "Potato", "Onion"],
-        answer: "Apples"
+        answer: "Apple"
     },
     {
         question: "What do we use to write?",
@@ -23,8 +23,6 @@ const questions = [
         question: "Which animal barks?",
         options: ["Cat", "Donkey", "Dog", "Lion"],
         answer: "Dog"
-    }, {
-        question: "Result is Pending"
     }
 ]
 const btnSecond = document.getElementById("btn-2");
@@ -33,19 +31,40 @@ const input = document.getElementById("input");
 const btn = document.getElementById("btn");
 
 let increamentQuestion = 0;
+let score = 0;
+
 function showQuestions() {
-    mcqQuestion.innerHTML = questions[increamentQuestion].question;
-    if (increamentQuestion === questions.length - 2) {
-        btn.innerHTML = "Check Result"
-    } else if (increamentQuestion === questions.length - 1) {
+    if (increamentQuestion !== 0) {
+        let selected = document.querySelector('input[name="option"]:checked');
+        if (selected === null || selected.value === '') {
+            alert("Please select this option");
+            return;
+        }
+
+        let checkAnswerQuestion = questions[increamentQuestion - 1];
+        if (selected.value === checkAnswerQuestion.answer) {
+            score++;
+        }
+    }
+
+
+    if (increamentQuestion === questions.length) {
+        mcqQuestion.innerHTML = `Your Score: ${score} out of ${questions.length}`;
         btn.style.display = "none";
         btnSecond.style.display = "block";
         input.innerHTML = "";
         return;
     }
-    btn.innerHTML = "Next"
+
+    mcqQuestion.innerHTML = questions[increamentQuestion].question;
+
+    if (increamentQuestion === questions.length - 1) {
+        btn.innerHTML = "Check Result"
+    } else {
+        btn.innerHTML = "Next"
+    }
     showOption();
-    nextQuestion();
+    increamentQuestion++
 
 }
 
@@ -53,37 +72,34 @@ function showOption() {
     const input = document.getElementById("input")
     input.innerHTML = ""
     for (var i = 0; i < questions[increamentQuestion].options.length; i++) {
-        input.innerHTML += `<input type="radio" name="option" id="${i}"> 
+        input.innerHTML += `<input type="radio" name="option" value="${questions[increamentQuestion].options[i]}" id="${i}"> 
         <label for="${i}">${questions[increamentQuestion].options[i]}</label> </br> </br>`
-    }
-}
-
-
-function nextQuestion() {
-    increamentQuestion++;
-    if (increamentQuestion === questions.length - 1) {
-        btn.innerHTML = "Check Result"
     }
 }
 
 function restartQuiz() {
     increamentQuestion = 0;
+    score = 0
     btnSecond.style.display = "none";
     btn.style.display = "block";
     btn.innerHTML = "Next";
+
     mcqQuestion.innerHTML = questions[increamentQuestion].question;
 
     if (questions[increamentQuestion].options) {
         input.innerHTML = "";
         for (let i = 0; i < questions[increamentQuestion].options.length; i++) {
             input.innerHTML += `
-                <input type="radio" name="option" id="${i}">
+                <input type="radio" name="option" value="${questions[increamentQuestion].options[i]}" id="${i}">
                 <label for="${i}">${questions[increamentQuestion].options[i]}</label><br><br>
             `;
         }
     }
     increamentQuestion++;
 }
+
+
+
 
 
 
